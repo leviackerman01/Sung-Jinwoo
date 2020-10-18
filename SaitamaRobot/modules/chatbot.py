13ceeada961bda18bfd848a3d941065fca4f3a69
Raki-12 <68501941+Raki-12@users.noreply.github.com>
2020-10-18 14:41:53 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -66,7 +66,7 @@ def remove_chat(update: Update, context: CallbackContext):
 
 def check_message(context: CallbackContext, message):
     reply_msg = message.reply_to_message
-    if message.text.lower() == "Mai Sakurajima":
+    if message.text.lower() == "mai sakurajima":
         return True
     if reply_msg:
         if reply_msg.from_user.id == context.bot.get_me().id:
