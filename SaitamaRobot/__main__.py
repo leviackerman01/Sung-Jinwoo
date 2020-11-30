c7f986ccad4617b7da2d8fc1ac4ef71778d0b18f
Raki-12 <68501941+Raki-12@users.noreply.github.com>
2020-11-30 20:02:05 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -52,7 +52,7 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.You can find my list of available commands with /help.
+I am an Anime themed group management bot. You can find my list of available commands with /help.
 """
 
 HELP_STRINGS = """
