31daaffa12329813d60bd49bc17859569927bbf8
Roronoa Zoro <73102278+Pirate-Hunter@users.noreply.github.com>
2020-12-03 21:24:51 +0530

    Update bans.py

--- SungJinwooRobot/modules/bans.py
+++ SungJinwooRobot/modules/bans.py
@@ -386,11 +386,11 @@ __help__ = """
 
 BAN_HANDLER = CommandHandler("ban", ban)
 TEMPBAN_HANDLER = CommandHandler(["tban"], temp_ban)
-PUNCH_HANDLER = CommandHandler("punch", punch)
+PUNCH_HANDLER = CommandHandler(("punch", "kick"), punch)
 UNBAN_HANDLER = CommandHandler("unban", unban)
 ROAR_HANDLER = CommandHandler("roar", selfunban)
-PUNCHME_HANDLER = DisableAbleCommandHandler(
-    "punchme", punchme, filters=Filters.group)
+PUNCHME_HANDLER = DisableAbleCommandHandler((
+    "punchme", "kickme", punchme, filters=Filters.group)
 
 dispatcher.add_handler(BAN_HANDLER)
 dispatcher.add_handler(TEMPBAN_HANDLER)
