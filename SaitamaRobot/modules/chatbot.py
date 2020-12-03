c46981b7e86b75ab4af33f7df495148da3f04327
Roronoa Zoro <ramakrishnan4510@gmail.com>
2020-12-03 10:57:10 +0530

    Removed reverse and modified backups and chatbot

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -70,7 +70,7 @@ def remove_chat(update: Update, context: CallbackContext):
 
 def check_message(context: CallbackContext, message):
     reply_msg = message.reply_to_message
-    if message.text.lower() == "mai sakurajima":
+    if message.text.lower() == "sung jin-woo":
         return True
     if reply_msg:
         if reply_msg.from_user.id == context.bot.get_me().id:
@@ -131,7 +131,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows Sung Jin-Woo to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 *Admins only:*
