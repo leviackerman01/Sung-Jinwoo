e72d9b0a4f50a851a01d7630a5b8ddaf38d63e31
leviackerman01 <79786572+leviackerman01@users.noreply.github.com>
2021-02-28 15:44:10 +0330

    Update sample_config.py

--- SungJinwooRobot/sample_config.py
+++ SungJinwooRobot/sample_config.py
@@ -18,11 +18,11 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 875506783  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Roronoa Zoro"
-    SUPPORT_CHAT = 'Hunters Associations'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1001480836884  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -1001480836884  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    OWNER_ID = 864358996  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "LEVI"
+    SUPPORT_CHAT = 'levisupport'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -1001307589753  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001307589753  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
